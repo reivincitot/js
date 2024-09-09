fa0f0f45282b5ab76483ee73d0d9ec1fc30a6324
Sebastian <reivincitot@gmail.com>
2024-09-09 14:48:37 -0300

    first commit

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,12 @@
+let nombre = "Hola Mundo";
+
+console.log(nombre);
+
+// Convenciones de los como formatear el nombre de las variables en js
+// UpperCamelCase Todas las primeras letras de una palabra comienzan con mayúscula
+let NombreCompleto;
+
+// camelCase la primera letra del nombre de la variable comienza con mayúscula y las siguientes letras de la palabra con mayúscula, esto es solo en caso de que sea un nombre compuesto de varias palabras
+let nombreCompleto;
+
+// snake_case 
